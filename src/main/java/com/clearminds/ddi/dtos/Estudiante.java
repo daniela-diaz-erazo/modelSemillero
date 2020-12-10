9c23284122b74068db39f8ed6f4eabb9c635c36a
Daniela Diaz <ampa.dde2106@gmail.com>
2020-12-10 10:04:54 -0500

    ejerc1 insertar y actualizar con campo edad

--- src/main/java/com/clearminds/ddi/dtos/Estudiante.java
+++ src/main/java/com/clearminds/ddi/dtos/Estudiante.java
@@ -5,20 +5,31 @@ public class Estudiante {
 	private String nombre;
 	private String apellido;
 	private int id;
-	public Estudiante(String nombre, String apellido, int id) {
+	private int edad;
+	public Estudiante(String nombre, String apellido, int id, int edad) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.id = id;
+		this.edad = edad;
 	}
-	public Estudiante(String nombre, String apellido) {
+	public Estudiante(String nombre, String apellido, int edad ) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad = edad;
 	}
+	
+	
 	@Override
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + ", edad=" + edad + "]";
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
 	}
 	public String getNombre() {
 		return nombre;
